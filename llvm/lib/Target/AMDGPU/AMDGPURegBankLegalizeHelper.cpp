e6066aec007c93096cce19829ec5be8f39d5d785
Lily Gorsheneva <bgorshenev@google.com>
2026-01-22 17:30:40 +0000

    Mark assert-only variable from 39642a4 maybe_unused.

--- llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeHelper.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegBankLegalizeHelper.cpp
@@ -683,7 +683,7 @@ bool RegBankLegalizeHelper::lowerSplitTo32(MachineInstr &MI) {
 
 bool RegBankLegalizeHelper::lowerSplitTo32Mul(MachineInstr &MI) {
   Register Dst = MI.getOperand(0).getReg();
-  LLT DstTy = MRI.getType(Dst);
+  [[maybe_unused]] LLT DstTy = MRI.getType(Dst);
   assert(DstTy == S64);
   auto Op1 = B.buildUnmerge({VgprRB_S32}, MI.getOperand(1).getReg());
   auto Op2 = B.buildUnmerge({VgprRB_S32}, MI.getOperand(2).getReg());
